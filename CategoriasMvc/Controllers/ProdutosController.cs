9b5e723dd0f8879c7ba391865c9e84d954f7c78e
paulohcarvalho07 <169151462+paulohcarvalho07@users.noreply.github.com>
2025-09-15 17:04:13 -0300

    Criando controlador de ProdutosController e endpoints CRUD juntamente com suas Views

--- CategoriasMvc/Controllers/ProdutosController.cs
+++ CategoriasMvc/Controllers/ProdutosController.cs
@@ -29,6 +29,95 @@ namespace CategoriasMvc.Controllers
 
             return View(result);
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> CriarNovoProduto()
+        {
+            ViewBag.CategoriaId =
+               new SelectList(await _categoriaService.GetCategorias(), "CategoriaId", "Nome");
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult<ProdutoViewModel>> CriarNovoProduto(ProdutoViewModel produtoVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _produtoService.CriaProduto(produtoVM, ObtemTokenJwt());
+
+                if (result != null)
+                    return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                ViewBag.CategoriaId =
+                new SelectList(await _categoriaService.GetCategorias(), "CategoriaId", "Nome");
+            }
+            return View(produtoVM);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DetalhesProduto(int id)
+        {
+            var produto = await _produtoService.GetProdutoPorId(id, ObtemTokenJwt());
+
+            if (produto is null)
+                return View("Error");
+
+            return View(produto);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> AtualizarProduto(int id)
+        {
+            var result = await _produtoService.GetProdutoPorId(id, ObtemTokenJwt());
+
+            if (result is null)
+                return View("Error");
+
+            ViewBag.CategoriaId =
+              new SelectList(await _categoriaService.GetCategorias(), "CategoriaId", "Nome");
+
+            return View(result);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ProdutoViewModel>> AtualizarProduto(int id, ProdutoViewModel produtoVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _produtoService.AtualizaProduto(id, produtoVM, ObtemTokenJwt());
+
+                if (result)
+                    return RedirectToAction(nameof(Index));
+            }
+            return View(produtoVM);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> DeletarProduto(int id)
+        {
+            var result = await _produtoService.GetProdutoPorId(id, ObtemTokenJwt());
+
+            if (result is null)
+                return View("Error");
+
+            return View(result);
+        }
+
+        [HttpPost(), ActionName("DeletarProduto")]
+        public async Task<IActionResult> DeletaConfirmado(int id)
+        {
+            var result = await _produtoService.DeletaProduto(id, ObtemTokenJwt());
+
+            if (result)
+                return RedirectToAction("Index");
+
+            return View("Error");
+        }
+
+
         private string ObtemTokenJwt()
         {
             if (HttpContext.Request.Cookies.ContainsKey("X-Access-Token"))
