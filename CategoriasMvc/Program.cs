e09f9db59afafac7debac12a26e2c37c6f232bae
paulohcarvalho07 <169151462+paulohcarvalho07@users.noreply.github.com>
2025-09-15 15:40:42 -0300

    Criando e implementando o serviço para consumir os endpoints da API

--- CategoriasMvc/Program.cs
+++ CategoriasMvc/Program.cs
@@ -18,6 +18,11 @@ builder.Services.AddHttpClient("AutenticaApi", c =>
     c.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 });
 
+builder.Services.AddHttpClient("ProdutosApi", c =>
+{
+    c.BaseAddress = new Uri(builder.Configuration["ServiceUri:ProdutosApi"]);
+});
+
 builder.Services.AddScoped<ICategoriaService, CategoriaService>();
 builder.Services.AddScoped<IAutenticacao, Autenticacao>();
 
